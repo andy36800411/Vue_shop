7d3470ece70e1364e7f75531abfa1eca10767ee7
Andy FU <andy36800411@gmail.com>
2021-09-07 10:08:40 +0200

    Terminé

--- src/main.js
+++ src/main.js
@@ -10,13 +10,21 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' 
 import 'quill/dist/quill.snow.css' 
 import 'quill/dist/quill.bubble.css' 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
-  console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
